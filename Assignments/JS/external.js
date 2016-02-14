088f9fe942177c4dc2995235c58c744b89dbe8f6
Akash Yadav <akash.yadav@tothenew.com>
2016-02-14 23:18:59 +0530

    secondcommit

---
+++ Assignments/JS/external.js
@@ -0,0 +1,22 @@
+		function change()
+		{
+			document.getElementById("d1").style.backgroundColor="red";
+		}
+
+
+		function rechange()
+		{
+			document.getElementById("d1").style.backgroundColor="yellow";
+		}
+
+		function change2()
+		{
+			document.getElementById("d2").style.backgroundColor="yellow";
+		}
+
+
+		function rechange2()
+		{
+			document.getElementById("d2").style.backgroundColor="red";
+		}
+
